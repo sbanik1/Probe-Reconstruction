69caeab3d57d9180195ef626e5b5b247ea908872
Swarnav Banik <sbanik1@umd.edu>
2020-12-12 11:21:39 -0500

    Restructured Repo

--- Test/2020/2020_11_24/StartupPR.m
+++ Test/2020/2020_11_24/StartupPR.m
@@ -10,20 +10,12 @@ function [FunctionsPath,DataFolderPath] =   StartupPR
     set(groot,'defaultAxesTickLabelInterpreter','latex');  
     set(groot,'defaultTextInterpreter','latex');
     set(groot,'defaultLegendInterpreter','latex');
-    set(0,'DefaultFigureWindowStyle','docked')
+    set(0,'DefaultFigureWindowStyle','docked');
+    set(groot,'defaultAxesFontSize',15);
+    set(groot,'defaultLegendFontSize',20);
 
     %% Computer Dependent Paths ===========================================
-    % Mac =================================================================
-    if ismac==1
-        mac = char(java.net.InetAddress.getLocalHost.getHostName);
-        if strcmp('MacBook-Pro',mac(1:11))==1
-            FunctionsPath = '/Users/swarnav/Google Drive/Work/Projects/Probe Reconstruction/Probe-Reconstruction/Functions/';
-            DataFolderPath = '/Users/swarnav/Google Drive/Work/Projects/Probe Reconstruction/Probe-Reconstruction/Test/';
-            set(groot,'defaultAxesFontSize',15);
-            set(groot,'defaultLegendFontSize',20);
-        else
-            fprintf('StartUpPR: this mac is not in the file\n')
-        end
-    end
+    FunctionsPath = '/Users/swarnav/Google Drive/Work/Projects/Probe Reconstruction/Probe-Reconstruction/Functions/';
+    DataFolderPath = '/Users/swarnav/Google Drive/Work/Projects/Probe Reconstruction/Probe-Reconstruction/Test/';
 
 end
